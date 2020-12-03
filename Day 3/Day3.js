87b0572f9383040a160da40e5872fb286169297d
Joris Willig <joris@Uptrends.com>
2020-12-03 17:16:11 +0100

    clean up code for day 3

--- Day 3/Day3.js
+++ Day 3/Day3.js
@@ -3,23 +3,19 @@ let input = [];
 
 function navigate(right, down) {
   const mapWidth = input[0].length;
-  console.log("mapWidth: " + mapWidth);
-  console.log("Map height: " + input.length);
   let xPos = 0;
   let treeCount = 0;
+
   for(let i = 0; i < input.length; i += down) {
     if (meetsTree(input[i], xPos)) treeCount++;
     xPos = (xPos + right) % mapWidth;
   }
-  console.log("Trees: " + treeCount)
+
   return treeCount;
 }
 
 function meetsTree(row, xPos) {
-  const isTree = row[xPos] === "#";
-  logRow = `${row.substring(0,xPos) + (isTree ? "X" : "O") + row.substring(xPos+1)}`;
-  console.log(logRow + " | " + isTree);
-  return isTree;
+  return row[xPos] === "#";
 }
 
 fileReader.readFile("./Day3.txt", "utf8", (err, data) => {
