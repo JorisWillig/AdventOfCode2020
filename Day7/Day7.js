e1ad0d685a7577caf97732f621c223e8d55f0507
Joris Willig <joris@Uptrends.com>
2020-12-07 14:38:14 +0100

    solved day 7 part 1

---
+++ Day7/Day7.js
@@ -0,0 +1,48 @@
+const fileReader = require("fs");
+let input = {};
+
+function findContent(rule, bagDescription) {
+  if (rule) {
+    const keys = Object.keys(rule)
+    for(let i = 0; i < keys.length; i++) {
+      if (keys[i] === bagDescription) return true;
+      if(findContent(input[keys[i]], bagDescription)) return true;
+    }
+  }
+  
+  return false;
+}
+
+function day7a() {
+  let ruleCount = 0;
+  Object.values(input).forEach((r) => {
+    if (findContent(r, "shiny gold")) ruleCount++;
+  });
+  console.log("You can put your 'shiny gold' bag in " + ruleCount + " different bags");
+}
+
+function day7b() {
+
+}
+
+fileReader.readFile("./Day7.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  const inputString = data
+  const rules = inputString.split("\r\n");
+  rules.forEach(r => {
+    const tempRule = r.split(" bags contain ");
+    const ruleName = tempRule[0];
+    tempRuleContents = tempRule[1].replace(".", "").split(", ");
+    const rule = {};
+    tempRuleContents.forEach((r) => {
+      contentsName = r.substring(2, r.length).replace(/ bag[s]?$/, "");
+      contentsCountString = r[0];
+      contentsCount = parseInt(contentsCountString);
+      if (!Number.isNaN(contentsCount)) rule[contentsName] = contentsCount
+    });
+    input[ruleName] = rule;
+  });
+
+  day7a();
+  day7b();
+});
