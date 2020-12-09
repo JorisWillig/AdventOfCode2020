de0abfca5d4f805fbf014809313526d506f57554
Joris Willig <joris@Uptrends.com>
2020-12-09 08:31:03 +0100

    Solved 8bgit add -A

--- Day8/Day8.js
+++ Day8/Day8.js
@@ -7,18 +7,21 @@ let shouldTry;
 let currentIndex;
 let accumulator;
 
-function executeCommand(command) {
+let commandTree = [];
+
+function executeCommand(command, acc) {
   switch (command.cmnd) {
     case "jmp":
       currentIndex += command.value;
-      return;
+      return acc;
     case "acc":
       accumulator += command.value;
+      acc += command.value;
       currentIndex++;
-      return;
+      return acc;
     default:
       currentIndex++;
-      return;
+      return acc;
   }
 }
 
@@ -38,11 +41,46 @@ function day8a() {
   console.log("accumulator: " + accumulator);
 }
 
-function day8b() {
-  
+function followCommandChain(index, acc, visited, shouldSplit) {
+  if (index === commandTree.length) return {success: true, acc};
+  if (visited.includes(index) || index < 0) return false;
+
+  visited.push(index);
+
+  const treeNode = commandTree[index];
+
+  let newAcc = executeCommand(treeNode.command, acc)
+  let result = followCommandChain(treeNode.child, newAcc, visited.slice(0), shouldSplit);
+  if (!result.success && shouldSplit && treeNode.canSplit) {
+    result = followCommandChain(treeNode.splitChild, newAcc, visited.slice(0), false);
+  }
+
+  return {success: result.success, acc: result.acc}
 }
 
-fileReader.readFile("./Day8-test.txt", "utf8", (err, data) => {
+function day8b() {
+  for(let i = 0; i < input.length; i++) {
+    currentCommand = input[i];
+    let canSplit = currentCommand.cmnd === "jmp" || currentCommand.cmnd === "nop";
+    const treeValue = {
+      command: currentCommand,
+      canSplit,
+      child: currentCommand.cmnd === "jmp" ? i + currentCommand.value : i + 1,
+      splitChild: canSplit && currentCommand === "nop" ? i + value : i + 1,
+    }
+    commandTree.push(treeValue);
+  }
+
+  const result = followCommandChain(0, 0, [], true);
+
+  if(result.success) {
+    console.log("Finished with accumulator: " + result.acc);
+  } else {
+    console.log("No correct path found.");
+  }
+}
+
+fileReader.readFile("./Day8.txt", "utf8", (err, data) => {
   if (err) throw err;
 
   const instructions = data.split("\r\n");
@@ -57,5 +95,5 @@ fileReader.readFile("./Day8-test.txt", "utf8", (err, data) => {
   console.log(input)
 
   day8a();
-  //day8b();
+  day8b();
 });
